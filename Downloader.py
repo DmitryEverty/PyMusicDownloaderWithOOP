a6e7d3bc1c2af854561cddb73f6be222fdea08e9
DmitryEverty <evertymusic@gmail.com>
2019-06-30 14:35:15 +0300

    link

--- Downloader.py
+++ Downloader.py
@@ -1,6 +1,6 @@
 import Classes
 
-lastTrack = 'https://www.frkmusic.cc/peace-quix-remix-alison-wonderland-mp3-320kbps-download-free/'
+lastTrack = 'https://www.frkmusic.cc/enter-ep-malou-prytz-mp3-320kbps-download-free/'
 musicSource = Classes.FrkMusic(lastTrack)
 
 musicSource.exportDownloadLinks()
